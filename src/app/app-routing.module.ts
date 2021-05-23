555be90317523a03780a6b8d353f04608b3cbb71
Celso Taliatelli <celsotaliatelli@hotmail.com>
2021-05-23 19:08:06 -0300

    Component de Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,9 +10,7 @@ const routes: Routes = [
   {
     path:'home',
     loadChildren: () => import('./home/home.module')
-    .then((m) => {
-      m.HomeModule
-    })
+    .then((m) => m.HomeModule)
   }
 ];
 
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,10 +1,15 @@
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [{
   path:'',
-  component:HomeComponent
+  component:HomeComponent,
+  children:[{
+    path:'',
+    component: LoginComponent,
+  }],
 }];
 
 @NgModule({
