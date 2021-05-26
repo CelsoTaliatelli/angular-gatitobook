fe8af360ea4eda177b8265b89e185820270e157f
Celso Taliatelli <celsotaliatelli@hotmail.com>
2021-05-26 14:41:32 -0300

    redirecionando rotas

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,5 +1,6 @@
 import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +12,12 @@ export class LoginComponent implements OnInit {
  usuario = '';
  senha =  '';
 
-  constructor(private authService: AutenticacaoService) { }
+  constructor(private authService: AutenticacaoService , private router: Router) { }
 
   login(){
     this.authService.autenticar(this.usuario,this.senha).subscribe(() => {
       console.log('autenticado');
+      this.router.navigate(['animais'])
     },(error) => {
       console.log('usuario ou senha invalidos');
     });
