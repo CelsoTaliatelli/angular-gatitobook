29b478fa4912b5a5d32c081f66aed39e81643c85
Celso Henrique Taliatelli <celsohenrique@bairral.local>
2021-05-31 14:27:38 -0300

    Formularios reativos

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,13 +5,15 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [HomeComponent, LoginComponent, NovoUsuarioComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ReactiveFormsModule
   ],
   exports:[
    HomeComponent
