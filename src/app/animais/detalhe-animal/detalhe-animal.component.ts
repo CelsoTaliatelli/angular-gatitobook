c2ea1e1f90a38d0ac7ccb838d689848556a73f41
Celso Taliatelli <celsotaliatelli@hotmail.com>
2021-06-14 19:54:55 -0300

    Serviço ActivetedRoute

--- src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -1,4 +1,8 @@
+import { AnimaisService } from './../animais.service';
+import { Animal } from './../animais';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalhe-animal',
@@ -7,9 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalheAnimalComponent implements OnInit {
 
-  constructor() { }
+  animalId!: number;
+  animal$!: Observable<Animal>;
+
+  constructor(private animaisService: AnimaisService,private activatedRoute: ActivatedRoute ){ }
 
   ngOnInit(): void {
+    this.animalId = this.activatedRoute.snapshot.params.animalId;
+    this.animal$ = this.animaisService.buscaPorId(this.animalId);
   }
 
 }
